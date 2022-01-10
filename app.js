9fa9c0ce8d80207f5d22112e89cb96151cfcb3f1
sergiogval <sergioval@gmail.com>
2022-01-10 17:32:57 -0600

    Fix linters

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-// eslint-disable rule-you-want-to-disable
-// eslint-disable-next-line no-unused-vars
-const myBooks = [
-  {
-    title: 'The Design of EveryDay Things',
-    author: 'Don Norman',
-  }, {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
-  }, {
-    title: '1984',
-    author: 'George Orwell',
-  }];
+import myBooks from './index.js';
 
-  const title = document.getElementById('title')
-  const author = document.getElementById('author')
-  const add = document.getElementById('add')
-  function saveLocalStore() {
-    const data = {
-      title: title.value,
-      author: author.value,
-    }
-    localStorage.setItem('data', JSON.stringify(data))
-  }
-  title.addEventListener('input', saveLocalStore)
-  author.addEventListener('input', saveLocalStore)
-  function getLocalStorage() {
-    if (localStorage.getItem('data')) {
-      const data = JSON.parse(localStorage.getItem('data'));
-      title.value = data.title
-      author.value = data.author
-    }
-  }
+const title = document.querySelectorAll('.title');
+const author = document.querySelectorAll('.author');
+const addBook = document.getElementById('addBook');
 
-  title.addEventListener('input', saveLocalStore)
-  author.addEventListener('input', saveLocalStore)
+window.addEventListener('load', () => {
+  if (localStorage.getItem('data')) {
+    const data = JSON.parse(localStorage.getItem('data'));
+    title.value = data.title;
+    author.value = data.author;
+  }
+});
+
+addBook.addEventListener('click', () => {
+  const data = {
+    title: title.value,
+    author: author.value,
+  };
+  myBooks.push('data');
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+// title.addEventListener('input', saveLocalStore);
+// author.addEventListener('input', saveLocalStore);
