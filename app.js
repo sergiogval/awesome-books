1c131f8c290dff6b31727b64f53b7a64eb6ee03e
sergiogval <sergioval@gmail.com>
2022-01-10 14:15:24 -0600

    Add feature

---
+++ app.js
@@ -0,0 +1,13 @@
+// eslint-disable rule-you-want-to-disable
+// eslint-disable-next-line no-unused-vars
+const myBooks = [
+  {
+    title: 'The Design of EveryDay Things',
+    author: 'Don Norman',
+  }, {
+    title: 'The Most Human Human',
+    author: 'Brian Christian',
+  }, {
+    title: '1984',
+    author: 'George Orwell',
+  }];
