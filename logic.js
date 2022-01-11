edfc085b0a7a5888b3949fd10361d7cbc2c312b7
Shay Craig <71286979+craigs40@users.noreply.github.com>
2022-01-11 16:13:55 -0500

    Refactor display and styles, add class and local storage

--- logic.js
+++ logic.js
@@ -1,89 +1,74 @@
-const myBooks = [
-  {
-    title: 'The Design of EveryDay Things',
-    author: 'Don Norman',
-  }, {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
-  }, {
-    title: '1984',
-    author: 'George Orwell',
-  }];
+const allBooks = document.querySelector('.all-books');
+const form = document.querySelector('.form');
+const [title, author] = form.elements;
 
-const form = document.getElementById('form');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const error = document.getElementById('error');
-const bookList = document.getElementById('bookList');
+const inputBook = {};
+let books = new Array([]);
 
-myBooks.forEach((elem) => {
-  const bookList = document.getElementById('bookList');
-  const bookItem = document.createElement('div');
-  bookItem.innerHTML = `
-  <div class="book"
-  <div>${elem.title}</div>
-  <div>${elem.author}</div>
-  <button type="button" class="remove">Remove</button>
-  <hr>
-  </div>`;
-  bookList.appendChild(bookItem);
+if (localStorage.savedBooks) {
+  books = JSON.parse(localStorage.getItem('savedBooks'));
+}
+
+title.addEventListener('change', () => {
+  inputBook.title = title.value;
 });
+
+author.addEventListener('change', () => {
+  inputBook.author = author.value;
+});
+
+const populateFields = () => {
+  localStorage.setItem('savedBooks', JSON.stringify(books));
+};
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static removeBook(book) {
+    const result = books.filter((b) => b !== book);
+    books = result;
+    populateFields();
+  }
+
+  static addBook = (newBook) => {
+    books.push(newBook);
+    populateFields();
+    this.displayBooks();
+  };
+
+  static displayBooks = () => {
+    allBooks.innerHTML = '';
+    books.map((book) => {
+      const bookDiv = document.createElement('tr');
+      const elementBook = document.createElement('td');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Remove';
+
+      elementBook.textContent = `"${book.title}" by ${book.author}`;
+
+      bookDiv.classList.add('book-container');
+      bookDiv.appendChild(elementBook);
+      bookDiv.appendChild(deleteBtn);
+
+      allBooks.appendChild(bookDiv);
+
+      deleteBtn.addEventListener('click', () => {
+        this.removeBook(book);
+        allBooks.removeChild(bookDiv);
+      });
+      return allBooks;
+    });
+  };
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  // eslint-disable-next-line no-use-before-define
-  addBooks();
-  // eslint-disable-next-line no-use-before-define
-  remove();
-  // eslint-disable-next-line no-use-before-define
-  saveLocalStore();
+  Book.addBook(new Book(inputBook.title, inputBook.author));
+  form.submit();
 });
 
-function addBooks() {
-  const titleText = title.value;
-  const authorText = author.value;
-  if (titleText === '' || authorText === '') {
-    error.textContent = 'Please fill in all fields';
-  } else {
-    const bookItem = document.createElement('div');
-    bookItem.innerHTML = `
-    <div class="book">
-      <div>${titleText}</div>
-      <div>${authorText}</div>
-      <button type="button" id="remove" class="remove">Remove</button>
-      <hr>
-    </div>`;
-    bookList.appendChild(bookItem);
-    // eslint-disable-next-line no-use-before-define
-    title.value = '';
-    author.value = '';
-  }
-  error.textContent = '';
-}
-
-function remove() {
-  const removeButton = document.querySelectorAll('.remove');
-  removeButton.forEach((elem) => {
-    elem.addEventListener('click', () => {
-      elem.parentNode.remove();
-    });
-  });
-}
-
-const book = {
-  // eslint-disable-next-line no-undef
-  title: `${titleText.value}`,
-  // eslint-disable-next-line no-undef
-  author: `${authorText.value}`,
-};
-// eslint-disable-next-line no-unused-vars
-function saveLocalStore() {
-  localStorage.setItem(book, JSON.stringify(book));
-}
-
-// eslint-disable-next-line no-unused-vars
-function getLocalStorage() {
-  localStorage.getItem('book', JSON.parse(book));
-  title.value = book.title;
-  author.value = book.author;
-}
+Book.displayBooks();
+populateFields();
